13e05393691643991ac9e1be4fccb93787e53727
A-Royal-official <octaviangrecu6@gmail.com>
2025-10-04 13:42:43 +0300

    Fix GitHub Pages routing - add base path and basename for React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Spotify">
       <App />
     </BrowserRouter>
   </React.StrictMode>
